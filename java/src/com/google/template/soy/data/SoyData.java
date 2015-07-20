9e9c9a5b302a1c6f5c12e00d35241f53c066149f
gboyer <gboyer@google.com>
2015-07-20 08:01:54 -0700

    Mark SoyData.createFromExistingData as deprecated and point to the alternatives. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=98639868

--- java/src/com/google/template/soy/data/SoyData.java
+++ java/src/com/google/template/soy/data/SoyData.java
@@ -37,33 +37,17 @@ public abstract class SoyData extends SoyAbstractValue {
 
 
   /**
-   * Creation function for creating a SoyData object out of any existing primitive, data object, or
-   * data structure.
-   *
-   * <p> Important: Avoid using this function if you know the type of the object at compile time.
-   * For example, if the object is a primitive, it can be passed directly to methods such as
-   * {@code SoyMapData.put()} or {@code SoyListData.add()}. If the object is a Map or an Iterable,
-   * you can directly create the equivalent SoyData object using the constructor of
-   * {@code SoyMapData} or {@code SoyListData}.
-   *
-   * <p> If the given object is already a SoyData object, then it is simply returned.
-   * Otherwise a new SoyData object will be created that is equivalent to the given primitive, data
-   * object, or data structure (even if the given object is null!).
-   *
-   * <p> Note that in order for the conversion process to succeed, the given data structure must
-   * correspond to a valid SoyData tree. Some requirements include:
-   * (a) all Maps within your data structure must have string keys that are identifiers,
-   * (b) all non-leaf nodes must be Maps or Lists,
-   * (c) all leaf nodes must be null, boolean, int, double, or String (corresponding to Soy
-   *     primitive data types null, boolean, integer, float, string).
-   *
-   * <p>Any object returned from this function should be regarded as immutable; Avoid downcasting
-   * to specific types unless you know what you're doing.
+   * Creates deprecated SoyData objects from standard Java data structures.
    *
    * @param obj The existing object or data structure to convert.
    * @return A SoyData object or tree that corresponds to the given object.
    * @throws SoyDataException If the given object cannot be converted to SoyData.
+   * @deprecated It's best to pass whatever object you have directly to the Soy templates you're
+   *     using -- Soy understands primitives, lists, and maps natively, and if you install runtime
+   *     support you can also pass protocol buffers. If you're interacting directly with the Soy
+   *     runtime and need SoyValue objects, use SoyValueHelper instead.
    */
+  @Deprecated
   public static SoyData createFromExistingData(Object obj) {
 
     // Important: This is frozen for backwards compatibility, For future changes (pun not intended),
