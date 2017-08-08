11131c98880f9e0f5901458389e38faac92fe6d7
Soy Authors <soy-dev@google.com>
2017-08-08 12:49:32 -0700

    Preserve the fully-qualified locale string for injection in soy. This is done by wrapping SoyMsgBundle in another layer that retains the original fully-qualified locale by which the message bundle was looked up.
    This locale is retained only if the translation's language code matches the fully-qualified locale's language code.
    
    #hr-localize-numbers
    
    CL history
    - Original []
    - Rolled back [] Breaking on the use of legacy "iw" language code in old Android phones instead of the modern "he".
    - Rollforward (this CL): Preserves deprecated language codes in bundles.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=164631496

---
+++ java/tests/com/google/template/soy/msgs/SoyMsgBundleWithFullLocaleTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.template.soy.msgs;
+
+import static org.junit.Assert.assertEquals;
+
+import com.ibm.icu.util.ULocale;
+import java.util.Locale;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class SoyMsgBundleWithFullLocaleTest {
+  @Test
+  public void testSoyMsgBundleWithFullLocale_compatibleLanguage() throws Exception {
+    // Given a locale whose language is compatible with SoyMsgBundle.EMPTY.
+    SoyMsgBundle bundle =
+        SoyMsgBundleWithFullLocale.preservingLocaleIfAllowed(SoyMsgBundle.EMPTY, Locale.US);
+    // Expect original locale in standard formatting.
+    assertEquals("en-US", bundle.getLocaleString());
+    // Even though the bundle is empty.
+    assertEquals(SoyMsgBundle.EMPTY, ((SoyMsgBundleWithFullLocale) bundle).getInnerSoyMsgBundle());
+  }
+
+  @Test
+  public void testSoyMsgBundleWithFullLocale_incompatibleLanguage() throws Exception {
+    // Given a locale whose language is incompatible with SoyMsgBundle.EMPTY.
+    SoyMsgBundle bundle =
+        SoyMsgBundleWithFullLocale.preservingLocaleIfAllowed(SoyMsgBundle.EMPTY, Locale.FRANCE);
+    // Expect SoyMsgBundle.EMPTY's locale.
+    assertEquals("en", bundle.getLocaleString());
+    // Even though the bundle is empty.
+    assertEquals(SoyMsgBundle.EMPTY, bundle);
+  }
+
+  @Test
+  public void testSoyMsgBundleWithFullLocale_deprecatedLanguageCode() throws Exception {
+    // Given a message bundle with a deprecated language code ("iw").
+    SoyMsgBundle iwBundle =
+        new SoyMsgBundleWithFullLocale(SoyMsgBundle.EMPTY, new ULocale("iw"), "iw");
+
+    // Expect the original soy bundle and locale to be preserved.
+    SoyMsgBundle bundle =
+        SoyMsgBundleWithFullLocale.preservingLocaleIfAllowed(iwBundle, new Locale("iw"));
+    assertEquals("iw", bundle.getLocaleString());
+    assertEquals(iwBundle, bundle);
+  }
+}
