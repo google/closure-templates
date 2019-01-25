e782cf7f5efc94a9c415465fc7454852e74b8da2
Soy Authors <soy-dev@google.com>
2019-01-25 14:09:39 -0800

    Add @suppress {checkTypes} back in. It was removed in cl/230900185 but there are a number of issues (tracked in b/11787791) with removing them.
    GITHUB_BREAKING_CHANGES=n/a, idom.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=230966029

--- testdata/javascript/soy_usegoog_lib.js
+++ testdata/javascript/soy_usegoog_lib.js
@@ -9488,6 +9488,13 @@ goog.labs.userAgent.platform.isKaiOS = function() {
 };
 
 /**
+ * @return {boolean} Whether the platform is Go2Phone.
+ */
+goog.labs.userAgent.platform.isGo2Phone = function() {
+  return goog.labs.userAgent.util.matchUserAgentIgnoreCase('GAFP');
+};
+
+/**
  * The version of the platform. We only determine the version for Windows,
  * Mac, and Chrome OS. It doesn't make much sense on Linux. For Windows, we only
  * look at the NT version. Non-NT-based versions (e.g. 95, 98, etc.) are given
@@ -9970,6 +9977,11 @@ goog.define('goog.userAgent.ASSUME_IPOD', false);
  */
 goog.define('goog.userAgent.ASSUME_KAIOS', false);
 
+/**
+ * @define {boolean} Whether the user agent is running on Go2Phone.
+ */
+goog.define('goog.userAgent.ASSUME_GO2PHONE', false);
+
 
 /**
  * @type {boolean}
@@ -10098,6 +10110,13 @@ goog.userAgent.IOS = goog.userAgent.PLATFORM_KNOWN_ ?
 goog.userAgent.KAIOS = goog.userAgent.PLATFORM_KNOWN_ ?
     goog.userAgent.ASSUME_KAIOS :
     goog.labs.userAgent.platform.isKaiOS();
+
+/**
+ * Whether the user agent is running on Go2Phone.
+ */
+goog.userAgent.GO2PHONE = goog.userAgent.PLATFORM_KNOWN_ ?
+    goog.userAgent.ASSUME_GO2PHONE :
+    goog.labs.userAgent.platform.isGo2Phone();
 
 
 /**
