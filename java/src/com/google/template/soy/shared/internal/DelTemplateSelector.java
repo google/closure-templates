90ee34a4a3875f5f402ceccfcf7e3026fe069011
lukes <lukes@google.com>
2019-02-22 10:14:58 -0800

    Rollforward cl/235111019 this time SoySauce itself is excluded and only the referenced types are included
    *** Original change description ***
    
    Make SoySauce statically verifiable as ThreadSafe
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=235212426

--- java/src/com/google/template/soy/shared/internal/DelTemplateSelector.java
+++ java/src/com/google/template/soy/shared/internal/DelTemplateSelector.java
@@ -25,6 +25,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Table;
 import com.google.common.collect.Tables;
+import com.google.errorprone.annotations.Immutable;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -43,6 +44,7 @@ import javax.annotation.Nullable;
  *
  * @param <T> The type of the values in the selector
  */
+@Immutable(containerOf = "T")
 public final class DelTemplateSelector<T> {
   private final ImmutableTable<String, String, Group<T>> nameAndVariantToGroup;
   private final ImmutableListMultimap<String, T> delTemplateNameToValues;
@@ -138,6 +140,7 @@ public final class DelTemplateSelector<T> {
   }
 
   /** Represents all the templates for a given deltemplate name and variant value. */
+  @Immutable(containerOf = "T")
   private static final class Group<T> {
     final String formattedName;
     @Nullable final T defaultValue;
