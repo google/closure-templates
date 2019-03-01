4d012be05ece307d24870608e2d95f3fd0e869ab
Soy Authors <soy-dev@google.com>
2019-03-01 08:31:07 -0800

    Remove visibility specs that haven't been used for at least 36 days.
    This CL looks good? Just LGTM and Approve it!
    This CL doesn't look good? File a bug from []
    What else can you do?
    * Suggest a fix on the CL ([]* Revert this CL, by replying "REVERT: <provide reason>".
    * Revert this CL and not get a CL that cleans up this file in the future from Scheuklappen (You will continue to receive CLs from other services) by replying "BLACKLIST: <provide reason>". This is not reversible!
    
    This CL was automatically generated by Scheuklappen ([]using CL Shepherd ([]
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=236318527

--- testdata/javascript/soy_usegoog_lib.js
+++ testdata/javascript/soy_usegoog_lib.js
@@ -4202,7 +4202,8 @@ goog.dom.NodeType = {
  *
  * The compiler has an option to disable asserts. So code like:
  * <code>
- * var x = goog.asserts.assert(foo()); goog.asserts.assert(bar());
+ * var x = goog.asserts.assert(foo());
+ * goog.asserts.assert(bar());
  * </code>
  * will be transformed into:
  * <code>
@@ -4210,6 +4211,14 @@ goog.dom.NodeType = {
  * </code>
  * The compiler will leave in foo() (because its return value is used),
  * but it will remove bar() because it assumes it does not have side-effects.
+ *
+ * Additionally, note the compiler will consider the type to be "tightened" for
+ * all statements <em>after</em> the assertion. For example:
+ * <code>
+ * const /** ?Object &#ast;/ value = foo();
+ * goog.asserts.assert(value);
+ * // "value" is of type {!Object} at this point.
+ * </code>
  *
  * @author agrieve@google.com (Andrew Grieve)
  */
