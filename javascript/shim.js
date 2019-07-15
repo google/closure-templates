db697478dece166884df3fa3774169e3ab8944a0
tomnguyen <tomnguyen@google.com>
2019-07-14 18:08:52 -0700

    Adjust the import path to indirectly refer to the OSS version of Incremental DOM, which does not use the tsickle path namespace
    GITHUB_BREAKING_CHANGES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=258074950

---
+++ javascript/shim.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Shim for OSS import to Incremental DOM.
+ */
+
+goog.module('google3.third_party.javascript.incremental_dom.index');
+
+const incrementaldom = goog.require('incrementaldom');
+
+exports = incrementaldom;
---
+++ javascript/types.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Shim for OSS import to Incremental DOM.
+ */
+
+goog.module('google3.third_party.javascript.incremental_dom.src.types');
+
+const types = goog.require('incrementaldom.src.types');
+
+exports = types;
