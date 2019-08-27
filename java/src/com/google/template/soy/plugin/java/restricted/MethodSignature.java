ee43b9b384ac259a47ed24b6dd907e443c8485ef
sameb <sameb@google.com>
2019-08-27 08:20:23 -0700

    Add a MethodSignature as an alternative to using Method for plugins to point to methods that will be called at runtime. This will allow plugins to refer to runtime logic without having the runtime code compiled in as a host dep.
    This isn't yet hooked up to anything, but will be used by subsequent CLs.
    
    GITHUB_BREAKING_CHANGES=none
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=265686911

---
+++ java/src/com/google/template/soy/plugin/java/restricted/MethodSignature.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.template.soy.plugin.java.restricted;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * The signature of a method. This allows identifying a method without requiring a compile-time
+ * dependency on its declaring class.
+ */
+@AutoValue
+public abstract class MethodSignature {
+
+  /**
+   * The fully qualified class name of the method, as returned by Class.getName.
+   *
+   * <p>This is the equivalent of {@link java.lang.reflect.Method#getDeclaringClass
+   * Method.getDeclaringClass}().{@link Class#getName getName}(), except it doesn't require a
+   * compile-time dependency on the declaring class.
+   */
+  public abstract String fullyQualifiedClassName();
+
+  /**
+   * The name of the method.
+   *
+   * <p>This is the equivalent of {@link java.lang.reflect.Method#getName}, except it doesn't
+   * require a compile-time dependency on the declaring class.
+   */
+  public abstract String methodName();
+
+  /**
+   * The return type of the method.
+   *
+   * <p>This is the equivalent of {@link java.lang.reflect.Method#getReturnType}, except it doesn't
+   * require a compile-time dependency on the declaring class.
+   */
+  public abstract Class<?> returnType();
+
+  /**
+   * The arguments of the method.
+   *
+   * <p>This is the equivalent of {@link java.lang.reflect.Method#getParameterTypes}, except it
+   * doesn't require a compile-time dependency on the declaring class.
+   */
+  public abstract ImmutableList<Class<?>> arguments();
+
+  MethodSignature() {} // package-private cxtor to users can't manually instantiate this.
+
+  /** Constructs a new MethodSignature. */
+  public static MethodSignature create(
+      String classFqn, String method, Class<?> returnType, Class<?>... args) {
+    return new AutoValue_MethodSignature(classFqn, method, returnType, ImmutableList.copyOf(args));
+  }
+}
