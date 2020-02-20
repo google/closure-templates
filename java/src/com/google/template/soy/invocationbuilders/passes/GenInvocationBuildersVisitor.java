1c608a8c6d54cf11ed3eb7d0ef9d5c27aa399ded
Soy Authors <soy-dev@google.com>
2020-02-19 16:46:24 -0800

    Add @Nullable to proto setters.
    GITHUB_BREAKING_CHANGES=NA
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=296087929

--- java/src/com/google/template/soy/invocationbuilders/passes/GenInvocationBuildersVisitor.java
+++ java/src/com/google/template/soy/invocationbuilders/passes/GenInvocationBuildersVisitor.java
@@ -38,8 +38,6 @@ import com.google.template.soy.base.internal.IndentedLinesBuilder;
 import com.google.template.soy.invocationbuilders.javatypes.CodeGenUtils;
 import com.google.template.soy.invocationbuilders.javatypes.FutureJavaType;
 import com.google.template.soy.invocationbuilders.javatypes.JavaType;
-import com.google.template.soy.invocationbuilders.javatypes.ProtoEnumJavaType;
-import com.google.template.soy.invocationbuilders.javatypes.ProtoJavaType;
 import com.google.template.soy.invocationbuilders.javatypes.RecordJavaType;
 import com.google.template.soy.invocationbuilders.passes.SoyFileNodeTransformer.FileInfo;
 import com.google.template.soy.invocationbuilders.passes.SoyFileNodeTransformer.ParamInfo;
@@ -511,13 +509,7 @@ public final class GenInvocationBuildersVisitor
       writeRecordSetter(ilb, param, (RecordJavaType) javaType);
     } else {
       String javaTypeString = javaType.toJavaTypeString();
-      // Add @Nullable if the type is nullable AND this isn't a proto/proto enum.
-      // TODO(b/140632665): Add fix for inserting @Nullable after proto package and before proto
-      // name.
-      boolean nullable =
-          javaType.isNullable()
-              && !(javaType instanceof ProtoEnumJavaType)
-              && !(javaType instanceof ProtoJavaType);
+      boolean nullable = javaType.isNullable();
 
       ilb.appendLine(
           "public Builder "
