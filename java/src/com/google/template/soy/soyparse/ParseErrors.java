cc96eede2dd81138464ce044002227d825202e50
lukes <lukes@google.com>
2020-06-19 11:33:42 -0700

    Fix an error recovery bug.
    If we failed parsing an expression in an if-statement we forgot to update the token marking the end of the if tag (the '}').  Then depending on where this occurred we would crash with an error that our tokens were in the wrong order.
    
    This is exacerbated by the fact that we fail to dump the error reporter in the case of an unexpected exception during parsing.  That issue will be addressed by [] which removes our hacky caching strategy.
    
    I also reviewed other locations where we called reportTemplateBodyErrorAndSkipTo to ensure we were doing the right thing and added a CheckReturnValue annotation so the compiler can help.
    
    Finally, i added some test coverage for this, which revealed a few more small bugs in error recovery.
    
    GITHUB_BREAKING_CHANGES=none
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=317346594

--- java/src/com/google/template/soy/soyparse/ParseErrors.java
+++ java/src/com/google/template/soy/soyparse/ParseErrors.java
@@ -113,7 +113,7 @@ final class ParseErrors {
           // "for" are reserved words.
           optionalAdvice +=
               ".\nNote: "
-                  + getSoyFileParserTokenDisplayName(errorToken.kind)
+                  + Tokens.getTokenDisplayName(errorToken.kind)
                   + " is a reserved word in soy.";
         }
         break;
@@ -150,7 +150,7 @@ final class ParseErrors {
     ImmutableSet.Builder<String> expectedTokenImages = ImmutableSet.builder();
     for (int[] expected : e.expectedTokenSequences) {
       // We only display the first token of any expected sequence
-      String displayName = getSoyFileParserTokenDisplayName(expected[0]);
+      String displayName = Tokens.getTokenDisplayName(expected[0]);
       if (displayName != null) {
         expectedTokenImages.add(displayName);
       }
@@ -161,106 +161,6 @@ final class ParseErrors {
         PLAIN_ERROR,
         formatParseExceptionDetails(errorToken.image, expectedTokenImages.build().asList())
             + optionalAdvice);
-  }
-
-  /**
-   * Returns a human friendly display name for tokens. By default we use the generated token image
-   * which is appropriate for literal tokens. or returns {@code null} if the name shouldn't be
-   * displayed.
-   */
-  private static String getSoyFileParserTokenDisplayName(int tokenId) {
-    switch (tokenId) {
-
-        // File-level tokens:
-      case SoyFileParserConstants.ALIAS_OPEN:
-        return "{alias";
-      case SoyFileParserConstants.DELPACKAGE_OPEN:
-        return "{delpackage";
-      case SoyFileParserConstants.DELTEMPLATE_OPEN:
-        return "{deltemplate";
-      case SoyFileParserConstants.NAMESPACE_OPEN:
-        return "{namespace";
-      case SoyFileParserConstants.TEMPLATE_OPEN:
-        return "{template";
-      case SoyFileParserConstants.ELEMENT_OPEN:
-        return "{element";
-      case SoyFileParserConstants.EOF:
-        return "eof";
-
-        // Template tokens:
-      case SoyFileParserConstants.CMD_BEGIN_CALL:
-        return "{call";
-      case SoyFileParserConstants.CMD_BEGIN_DELCALL:
-        return "{delcall";
-      case SoyFileParserConstants.CMD_BEGIN_PARAM:
-        return "{param";
-      case SoyFileParserConstants.CMD_BEGIN_MSG:
-        return "{msg";
-      case SoyFileParserConstants.CMD_BEGIN_FALLBACK_MSG:
-        return "{fallbackmsg";
-      case SoyFileParserConstants.CMD_BEGIN_IF:
-        return "{if";
-      case SoyFileParserConstants.CMD_BEGIN_ELSEIF:
-        return "{elseif";
-      case SoyFileParserConstants.CMD_BEGIN_LET:
-        return "{let";
-      case SoyFileParserConstants.CMD_BEGIN_FOR:
-        return "{for";
-      case SoyFileParserConstants.CMD_BEGIN_PLURAL:
-        return "{plural";
-      case SoyFileParserConstants.CMD_BEGIN_SELECT:
-        return "{select";
-      case SoyFileParserConstants.CMD_BEGIN_SWITCH:
-        return "{switch";
-      case SoyFileParserConstants.CMD_BEGIN_CASE:
-        return "{case";
-      case SoyFileParserConstants.CMD_BEGIN_PRINT:
-        return "{print";
-      case SoyFileParserConstants.CMD_BEGIN_KEY:
-        return "{key";
-      case SoyFileParserConstants.CMD_BEGIN_VELOG:
-        return "{velog";
-      case SoyFileParserConstants.NAME:
-      case SoyFileParserConstants.IDENT:
-        return "identifier";
-      case SoyFileParserConstants.SQ_ATTRIBUTE_VALUE:
-      case SoyFileParserConstants.DQ_ATTRIBUTE_VALUE:
-        return "attribute value";
-
-      case SoyFileParserConstants.CMD_FULL_SP:
-      case SoyFileParserConstants.CMD_FULL_NIL:
-      case SoyFileParserConstants.CMD_FULL_CR:
-      case SoyFileParserConstants.CMD_FULL_LF:
-      case SoyFileParserConstants.CMD_FULL_TAB:
-      case SoyFileParserConstants.CMD_FULL_LB:
-      case SoyFileParserConstants.CMD_FULL_RB:
-      case SoyFileParserConstants.CMD_FULL_NBSP:
-      case SoyFileParserConstants.TOKEN_NOT_WS:
-        return "text";
-      case SoyFileParserConstants.TOKEN_WS:
-        return "whitespace";
-
-        // Expression tokens:
-      case SoyFileParserConstants.HEX_INTEGER:
-      case SoyFileParserConstants.DEC_INTEGER:
-      case SoyFileParserConstants.FLOAT:
-        return "number";
-      case SoyFileParserConstants.SINGLE_QUOTE:
-      case SoyFileParserConstants.DOUBLE_QUOTE:
-        return "string";
-      case SoyFileParserConstants.DOLLAR_IDENT:
-        return "variable";
-      case SoyFileParserConstants.FOR:
-        return "\'for\'";
-      case SoyFileParserConstants.IN:
-        return "\'in\'";
-
-      case SoyFileParserConstants.UNEXPECTED_TOKEN:
-        throw new AssertionError("we should never expect the unexpected token");
-
-      default:
-        return SoyFileParserConstants.tokenImage[tokenId];
-    }
   }
 
   static void reportTokenMgrError(
@@ -326,7 +226,7 @@ final class ParseErrors {
     }
 
     return String.format(
-        "parse error at '%s'%s", escapeWhitespaceForErrorPrinting(errorToken), details.toString());
+        "parse error at '%s'%s", escapeWhitespaceForErrorPrinting(errorToken), details);
   }
 
   private static String maybeQuoteForParseError(String token) {
--- java/src/com/google/template/soy/soyparse/Tokens.java
+++ java/src/com/google/template/soy/soyparse/Tokens.java
@@ -16,8 +16,7 @@
 
 package com.google.template.soy.soyparse;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
+import com.google.common.base.MoreObjects;
 import com.google.template.soy.base.SourceLocation;
 
 /** Helpers for dealing with {@link Token tokens} */
@@ -35,8 +34,19 @@ final class Tokens {
     int endColumn = first.endColumn;
 
     for (Token next : rest) {
-      checkArgument(startsLaterThan(next, beginLine, beginColumn));
-      checkArgument(endsLaterThan(next, endLine, endColumn));
+      if (!startsLaterThan(next, beginLine, beginColumn)) {
+        throw new IllegalArgumentException(
+            String.format(
+                "In file: %s, expected %s to start after (%d, %d)",
+                filePath, toString(next), beginLine, beginColumn));
+      }
+      if (!endsLaterThan(next, endLine, endColumn)) {
+        throw new IllegalArgumentException(
+            String.format(
+                "In file: %s, expected %s to end after (%d, %d)",
+                filePath, toString(next), endLine, endColumn));
+      }
+      ;
       endLine = next.endLine;
       endColumn = next.endColumn;
     }
@@ -61,6 +71,117 @@ final class Tokens {
    */
   static boolean areAdjacent(Token first, Token second) {
     return first.endLine == second.beginLine && first.endColumn == second.beginColumn - 1;
+  }
+
+  static String toString(Token token) {
+    return MoreObjects.toStringHelper(Token.class)
+        .add("type", getTokenDisplayName(token.kind))
+        .add("image", token.image)
+        .add("beginLine", token.beginLine)
+        .add("beginColumn", token.beginColumn)
+        .add("endLine", token.endLine)
+        .add("endColumn", token.endColumn)
+        .toString();
+  }
+
+  /**
+   * Returns a human friendly display name for tokens. By default we use the generated token image
+   * which is appropriate for literal tokens. or returns {@code null} if the name shouldn't be
+   * displayed.
+   */
+  static String getTokenDisplayName(int tokenId) {
+    switch (tokenId) {
+
+        // File-level tokens:
+      case SoyFileParserConstants.ALIAS_OPEN:
+        return "{alias";
+      case SoyFileParserConstants.DELPACKAGE_OPEN:
+        return "{delpackage";
+      case SoyFileParserConstants.DELTEMPLATE_OPEN:
+        return "{deltemplate";
+      case SoyFileParserConstants.NAMESPACE_OPEN:
+        return "{namespace";
+      case SoyFileParserConstants.TEMPLATE_OPEN:
+        return "{template";
+      case SoyFileParserConstants.ELEMENT_OPEN:
+        return "{element";
+      case SoyFileParserConstants.EOF:
+        return "eof";
+
+        // Template tokens:
+      case SoyFileParserConstants.CMD_BEGIN_CALL:
+        return "{call";
+      case SoyFileParserConstants.CMD_BEGIN_DELCALL:
+        return "{delcall";
+      case SoyFileParserConstants.CMD_BEGIN_PARAM:
+        return "{param";
+      case SoyFileParserConstants.CMD_BEGIN_MSG:
+        return "{msg";
+      case SoyFileParserConstants.CMD_BEGIN_FALLBACK_MSG:
+        return "{fallbackmsg";
+      case SoyFileParserConstants.CMD_BEGIN_IF:
+        return "{if";
+      case SoyFileParserConstants.CMD_BEGIN_ELSEIF:
+        return "{elseif";
+      case SoyFileParserConstants.CMD_BEGIN_LET:
+        return "{let";
+      case SoyFileParserConstants.CMD_BEGIN_FOR:
+        return "{for";
+      case SoyFileParserConstants.CMD_BEGIN_PLURAL:
+        return "{plural";
+      case SoyFileParserConstants.CMD_BEGIN_SELECT:
+        return "{select";
+      case SoyFileParserConstants.CMD_BEGIN_SWITCH:
+        return "{switch";
+      case SoyFileParserConstants.CMD_BEGIN_CASE:
+        return "{case";
+      case SoyFileParserConstants.CMD_BEGIN_PRINT:
+        return "{print";
+      case SoyFileParserConstants.CMD_BEGIN_KEY:
+        return "{key";
+      case SoyFileParserConstants.CMD_BEGIN_VELOG:
+        return "{velog";
+      case SoyFileParserConstants.NAME:
+      case SoyFileParserConstants.IDENT:
+        return "identifier";
+      case SoyFileParserConstants.SQ_ATTRIBUTE_VALUE:
+      case SoyFileParserConstants.DQ_ATTRIBUTE_VALUE:
+        return "attribute value";
+
+      case SoyFileParserConstants.CMD_FULL_SP:
+      case SoyFileParserConstants.CMD_FULL_NIL:
+      case SoyFileParserConstants.CMD_FULL_CR:
+      case SoyFileParserConstants.CMD_FULL_LF:
+      case SoyFileParserConstants.CMD_FULL_TAB:
+      case SoyFileParserConstants.CMD_FULL_LB:
+      case SoyFileParserConstants.CMD_FULL_RB:
+      case SoyFileParserConstants.CMD_FULL_NBSP:
+      case SoyFileParserConstants.TOKEN_NOT_WS:
+        return "text";
+      case SoyFileParserConstants.TOKEN_WS:
+        return "whitespace";
+
+        // Expression tokens:
+      case SoyFileParserConstants.HEX_INTEGER:
+      case SoyFileParserConstants.DEC_INTEGER:
+      case SoyFileParserConstants.FLOAT:
+        return "number";
+      case SoyFileParserConstants.SINGLE_QUOTE:
+      case SoyFileParserConstants.DOUBLE_QUOTE:
+        return "string";
+      case SoyFileParserConstants.DOLLAR_IDENT:
+        return "variable";
+      case SoyFileParserConstants.FOR:
+        return "\'for\'";
+      case SoyFileParserConstants.IN:
+        return "\'in\'";
+
+      case SoyFileParserConstants.UNEXPECTED_TOKEN:
+        throw new AssertionError("we should never expect the unexpected token");
+
+      default:
+        return SoyFileParserConstants.tokenImage[tokenId];
+    }
   }
 
   private Tokens() {}
