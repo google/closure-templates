214d99ff78fb9a11e77e99030205048a53f93540
Soy Authors <soy-dev@google.com>
2020-06-23 10:15:14 -0700

    Allow aliases collisions when identical to proto imports. Makes multipass proto tricorder more resilient.
    GITHUB_BREAKING_CHANGES=N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=317890583

--- java/src/com/google/template/soy/passes/ValidateAliasesPass.java
+++ java/src/com/google/template/soy/passes/ValidateAliasesPass.java
@@ -23,6 +23,8 @@ import com.google.template.soy.logging.ValidatedLoggingConfig;
 import com.google.template.soy.shared.SoyGeneralOptions;
 import com.google.template.soy.soytree.AliasDeclaration;
 import com.google.template.soy.soytree.SoyFileNode;
+import com.google.template.soy.types.SoyProtoEnumType;
+import com.google.template.soy.types.SoyProtoType;
 import com.google.template.soy.types.SoyType;
 import com.google.template.soy.types.SoyTypeRegistry;
 
@@ -66,8 +68,12 @@ final class ValidateAliasesPass implements CompilerFilePass {
       SoyType type = registry.getType(alias.alias().identifier());
       // When running with a dummy type provider that parses all types as unknown, ignore that.
       if (type != null && type.getKind() != SoyType.Kind.UNKNOWN) {
-        errorReporter.report(
-            alias.alias().location(), ALIAS_CONFLICTS_WITH_TYPE_NAME, alias.alias());
+        // Temporarily, while we migrate all proto aliases to imports, ignore aliases that are
+        // identical to proto imports. They don't conflict and Tricorder will remove the aliases.
+        if (!alias.namespace().identifier().equals(getFqProtoName(type))) {
+          errorReporter.report(
+              alias.alias().location(), ALIAS_CONFLICTS_WITH_TYPE_NAME, alias.alias());
+        }
       }
       String conflictingNamespacedType =
           registry.findTypeWithMatchingNamespace(alias.alias().identifier());
@@ -88,6 +94,16 @@ final class ValidateAliasesPass implements CompilerFilePass {
       if (loggingConfig.getElement(alias.alias().identifier()) != null) {
         errorReporter.report(alias.alias().location(), ALIAS_CONFLICTS_WITH_VE, alias.alias());
       }
+    }
+  }
+
+  private static String getFqProtoName(SoyType type) {
+    if (type instanceof SoyProtoType) {
+      return ((SoyProtoType) type).getDescriptor().getFullName();
+    } else if (type instanceof SoyProtoEnumType) {
+      return ((SoyProtoEnumType) type).getDescriptor().getFullName();
+    } else {
+      return null;
     }
   }
 }
