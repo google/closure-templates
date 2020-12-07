d3dbd7943bfa078212d1d00ad115f5e7dadb03ec
nickreid <nickreid@google.com>
2020-12-07 11:28:24 -0800

    Migrate files containing a single goog.provide to use goog.module + goog.module.declareLegacyNamespace
    goog.module has the following main advantages:
      - []
      - short import aliases
      - better compile-time checks
      - [goog.provide is deprecated]([]
    
    However modules also have different semantics that can cause problems in code with unsupported patterns:
      - modules execute in [strict mode]([]
        - referencing undeclared variables is an error
        - some changes to `eval` and `arguments`
        - etc.
      - top-level variables are scoped to the module body, rather than being globals
    
    []
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=346138865

--- javascript/jspbconversions.js
+++ javascript/jspbconversions.js
@@ -4,26 +4,27 @@
  *
  */
 
-goog.provide('security.html.jspbconversions');
+goog.module('security.html.jspbconversions');
+goog.module.declareLegacyNamespace();
 
-goog.require('goog.html.SafeHtml');
-goog.require('goog.html.SafeScript');
-goog.require('goog.html.SafeStyle');
-goog.require('goog.html.SafeStyleSheet');
-goog.require('goog.html.SafeUrl');
-goog.require('goog.html.TrustedResourceUrl');
-goog.require('goog.html.uncheckedconversions');
-goog.require('goog.string.Const');
-goog.require('proto.webutil.html.types.SafeHtmlProto');
-goog.require('proto.webutil.html.types.SafeScriptProto');
-goog.require('proto.webutil.html.types.SafeStyleProto');
-goog.require('proto.webutil.html.types.SafeStyleSheetProto');
-goog.require('proto.webutil.html.types.SafeUrlProto');
-goog.require('proto.webutil.html.types.TrustedResourceUrlProto');
+const Const = goog.require('goog.string.Const');
+const SafeHtml = goog.require('goog.html.SafeHtml');
+const SafeHtmlProto = goog.require('proto.webutil.html.types.SafeHtmlProto');
+const SafeScript = goog.require('goog.html.SafeScript');
+const SafeScriptProto = goog.require('proto.webutil.html.types.SafeScriptProto');
+const SafeStyle = goog.require('goog.html.SafeStyle');
+const SafeStyleProto = goog.require('proto.webutil.html.types.SafeStyleProto');
+const SafeStyleSheet = goog.require('goog.html.SafeStyleSheet');
+const SafeStyleSheetProto = goog.require('proto.webutil.html.types.SafeStyleSheetProto');
+const SafeUrl = goog.require('goog.html.SafeUrl');
+const SafeUrlProto = goog.require('proto.webutil.html.types.SafeUrlProto');
+const TrustedResourceUrl = goog.require('goog.html.TrustedResourceUrl');
+const TrustedResourceUrlProto = goog.require('proto.webutil.html.types.TrustedResourceUrlProto');
+const uncheckedconversions = goog.require('goog.html.uncheckedconversions');
 
 /**
- * Converts a protocol-message form of a goog.html.SafeHtml into a
- * goog.html.SafeHtml instance.
+ * Converts a protocol-message form of a SafeHtml into a
+ * SafeHtml instance.
  *
  * Protocol-message forms are intended to be opaque. The fields of the
  * protocol message should be considered encapsulated and are not intended for
@@ -35,20 +36,18 @@ goog.require('proto.webutil.html.types.TrustedResourceUrlProto');
  * message that has been received from an entity outside the application's
  * trust domain. Data coming from the browser is outside the application's trust
  * domain.
- * @param {!proto.webutil.html.types.SafeHtmlProto} proto Protocol message to
+ * @param {!SafeHtmlProto} proto Protocol message to
  *   convert from.
- * @return {!goog.html.SafeHtml}
+ * @return {!SafeHtml}
  */
-security.html.jspbconversions.safeHtmlFromProto = function(proto) {
-  return goog.html.uncheckedconversions.
-      safeHtmlFromStringKnownToSatisfyTypeContract(
-          goog.string.Const.from('From proto message. b/12014412'),
-          proto.getPrivateDoNotAccessOrElseSafeHtmlWrappedValue() || '');
-};
-
+function safeHtmlFromProto(proto) {
+  return uncheckedconversions.safeHtmlFromStringKnownToSatisfyTypeContract(
+      Const.from('From proto message. b/12014412'),
+      proto.getPrivateDoNotAccessOrElseSafeHtmlWrappedValue() || '');
+}
 
 /**
- * Returns a protocol message representation of the given goog.html.SafeHtml.
+ * Returns a protocol message representation of the given SafeHtml.
  *
  * Such a representation lets the SafeHtml type be preserved across protocol
  * buffer serialization and deserialization.
@@ -57,21 +56,20 @@ security.html.jspbconversions.safeHtmlFromProto = function(proto) {
  * of the returned protocol message should be considered encapsulated and are
  * not intended for direct inspection or manipulation. Protocol messages can
  * be converted back into an instance of this type using
- * security.html.jspbconversions.safeHtmlFromProto.
- * @param {!goog.html.SafeHtml} safehtml SafeHtml to serialize.
- * @return {!proto.webutil.html.types.SafeHtmlProto}
+ * safeHtmlFromProto.
+ * @param {!SafeHtml} safehtml SafeHtml to serialize.
+ * @return {!SafeHtmlProto}
  */
-security.html.jspbconversions.safeHtmlToProto = function(safehtml) {
-  var protoHtml = new proto.webutil.html.types.SafeHtmlProto();
-  var string = goog.html.SafeHtml.unwrap(safehtml);
+function safeHtmlToProto(safehtml) {
+  var protoHtml = new SafeHtmlProto();
+  var string = SafeHtml.unwrap(safehtml);
   protoHtml.setPrivateDoNotAccessOrElseSafeHtmlWrappedValue(string);
   return protoHtml;
-};
-
+}
 
 /**
- * Converts a protocol-message form of a goog.html.SafeScript into a
- * goog.html.SafeScript instance.
+ * Converts a protocol-message form of a SafeScript into a
+ * SafeScript instance.
  *
  * Protocol-message forms are intended to be opaque. The fields of the
  * protocol message should be considered encapsulated and are not intended for
@@ -83,21 +81,19 @@ security.html.jspbconversions.safeHtmlToProto = function(safehtml) {
  * message that has been received from an entity outside the application's
  * trust domain. Data coming from the browser is outside the application's trust
  * domain.
- * @param {!proto.webutil.html.types.SafeScriptProto} proto Protocol message to
+ * @param {!SafeScriptProto} proto Protocol message to
  *   convert from.
- * @return {!goog.html.SafeScript}
+ * @return {!SafeScript}
  * @suppress {visibility}
  */
-security.html.jspbconversions.safeScriptFromProto = function(proto) {
-  return goog.html.uncheckedconversions
-      .safeScriptFromStringKnownToSatisfyTypeContract(
-          goog.string.Const.from('From proto message. b/12014412'),
-          proto.getPrivateDoNotAccessOrElseSafeScriptWrappedValue() || '');
-};
-
+function safeScriptFromProto(proto) {
+  return uncheckedconversions.safeScriptFromStringKnownToSatisfyTypeContract(
+      Const.from('From proto message. b/12014412'),
+      proto.getPrivateDoNotAccessOrElseSafeScriptWrappedValue() || '');
+}
 
 /**
- * Returns a protocol message representation of the given goog.html.SafeScript.
+ * Returns a protocol message representation of the given SafeScript.
  *
  * Such a representation lets the SafeScript type be preserved across protocol
  * buffer serialization and deserialization.
@@ -106,21 +102,20 @@ security.html.jspbconversions.safeScriptFromProto = function(proto) {
  * of the returned protocol message should be considered encapsulated and are
  * not intended for direct inspection or manipulation. Protocol messages can
  * be converted back into an instance of this type using
- * security.html.jspbconversions.safeScriptFromProto.
- * @param {!goog.html.SafeScript} script SafeScript to serialize.
- * @return {!proto.webutil.html.types.SafeScriptProto}
+ * safeScriptFromProto.
+ * @param {!SafeScript} script SafeScript to serialize.
+ * @return {!SafeScriptProto}
  */
-security.html.jspbconversions.safeScriptToProto = function(script) {
-  var protoScript = new proto.webutil.html.types.SafeScriptProto();
-  var string = goog.html.SafeScript.unwrap(script);
+function safeScriptToProto(script) {
+  var protoScript = new SafeScriptProto();
+  var string = SafeScript.unwrap(script);
   protoScript.setPrivateDoNotAccessOrElseSafeScriptWrappedValue(string);
   return protoScript;
-};
-
+}
 
 /**
- * Converts a protocol-message form of a goog.html.SafeStyle into a
- * goog.html.SafeStyle instance.
+ * Converts a protocol-message form of a SafeStyle into a
+ * SafeStyle instance.
  *
  * Protocol-message forms are intended to be opaque. The fields of the
  * protocol message should be considered encapsulated and are not intended for
@@ -132,21 +127,19 @@ security.html.jspbconversions.safeScriptToProto = function(script) {
  * message that has been received from an entity outside the application's
  * trust domain. Data coming from the browser is outside the application's trust
  * domain.
- * @param {!proto.webutil.html.types.SafeStyleProto} proto Protocol message to
+ * @param {!SafeStyleProto} proto Protocol message to
  *   convert from.
- * @return {!goog.html.SafeStyle}
+ * @return {!SafeStyle}
  * @suppress {visibility}
  */
-security.html.jspbconversions.safeStyleFromProto = function(proto) {
-  return goog.html.uncheckedconversions.
-      safeStyleFromStringKnownToSatisfyTypeContract(
-          goog.string.Const.from('From proto message. b/12014412'),
-          proto.getPrivateDoNotAccessOrElseSafeStyleWrappedValue() || '');
-};
-
+function safeStyleFromProto(proto) {
+  return uncheckedconversions.safeStyleFromStringKnownToSatisfyTypeContract(
+      Const.from('From proto message. b/12014412'),
+      proto.getPrivateDoNotAccessOrElseSafeStyleWrappedValue() || '');
+}
 
 /**
- * Returns a protocol message representation of the given goog.html.SafeStyle.
+ * Returns a protocol message representation of the given SafeStyle.
  *
  * Such a representation lets the SafeStyle type be preserved across protocol
  * buffer serialization and deserialization.
@@ -155,21 +148,20 @@ security.html.jspbconversions.safeStyleFromProto = function(proto) {
  * of the returned protocol message should be considered encapsulated and are
  * not intended for direct inspection or manipulation. Protocol messages can
  * be converted back into an instance of this type using
- * security.html.jspbconversions.safeStyleFromProto.
- * @param {!goog.html.SafeStyle} style SafeStyle to serialize.
- * @return {!proto.webutil.html.types.SafeStyleProto}
+ * safeStyleFromProto.
+ * @param {!SafeStyle} style SafeStyle to serialize.
+ * @return {!SafeStyleProto}
  */
-security.html.jspbconversions.safeStyleToProto = function(style) {
-  var protoStyle = new proto.webutil.html.types.SafeStyleProto();
-  var string = goog.html.SafeStyle.unwrap(style);
+function safeStyleToProto(style) {
+  var protoStyle = new SafeStyleProto();
+  var string = SafeStyle.unwrap(style);
   protoStyle.setPrivateDoNotAccessOrElseSafeStyleWrappedValue(string);
   return protoStyle;
-};
-
+}
 
 /**
- * Converts a protocol-message form of a goog.html.SafeStyleSheet into a
- * goog.html.SafeStyleSheet instance.
+ * Converts a protocol-message form of a SafeStyleSheet into a
+ * SafeStyleSheet instance.
  *
  * Protocol-message forms are intended to be opaque. The fields of the
  * protocol message should be considered encapsulated and are not intended for
@@ -181,22 +173,21 @@ security.html.jspbconversions.safeStyleToProto = function(style) {
  * message that has been received from an entity outside the application's
  * trust domain. Data coming from the browser is outside the application's trust
  * domain.
- * @param {!proto.webutil.html.types.SafeStyleSheetProto} proto Protocol message to
+ * @param {!SafeStyleSheetProto} proto Protocol message to
  *   convert from.
- * @return {!goog.html.SafeStyleSheet}
+ * @return {!SafeStyleSheet}
  * @suppress {visibility}
  */
-security.html.jspbconversions.safeStyleSheetFromProto = function(proto) {
-  return goog.html.uncheckedconversions
+function safeStyleSheetFromProto(proto) {
+  return uncheckedconversions
       .safeStyleSheetFromStringKnownToSatisfyTypeContract(
-          goog.string.Const.from('From proto message. b/12014412'),
+          Const.from('From proto message. b/12014412'),
           proto.getPrivateDoNotAccessOrElseSafeStyleSheetWrappedValue() || '');
-};
-
+}
 
 /**
  * Returns a protocol message representation of the given
- * goog.html.SafeStyleSheet.
+ * SafeStyleSheet.
  *
  * Such a representation lets the SafeStyleSheet type be preserved across
  * protocol buffer serialization and deserialization.
@@ -205,21 +196,20 @@ security.html.jspbconversions.safeStyleSheetFromProto = function(proto) {
  * of the returned protocol message should be considered encapsulated and are
  * not intended for direct inspection or manipulation. Protocol messages can
  * be converted back into an instance of this type using
- * security.html.jspbconversions.safeStyleSheetFromProto.
- * @param {!goog.html.SafeStyleSheet} styleSheet SafeStyleSheet to serialize.
- * @return {!proto.webutil.html.types.SafeStyleSheetProto}
+ * safeStyleSheetFromProto.
+ * @param {!SafeStyleSheet} styleSheet SafeStyleSheet to serialize.
+ * @return {!SafeStyleSheetProto}
  */
-security.html.jspbconversions.safeStyleSheetToProto = function(styleSheet) {
-  var protoStyleSheet = new proto.webutil.html.types.SafeStyleSheetProto();
-  var string = goog.html.SafeStyleSheet.unwrap(styleSheet);
+function safeStyleSheetToProto(styleSheet) {
+  var protoStyleSheet = new SafeStyleSheetProto();
+  var string = SafeStyleSheet.unwrap(styleSheet);
   protoStyleSheet.setPrivateDoNotAccessOrElseSafeStyleSheetWrappedValue(string);
   return protoStyleSheet;
-};
-
+}
 
 /**
- * Converts a protocol-message form of a goog.html.SafeUrl into a
- * goog.html.SafeUrl instance.
+ * Converts a protocol-message form of a SafeUrl into a
+ * SafeUrl instance.
  *
  * Protocol-message forms are intended to be opaque. The fields of the
  * protocol message should be considered encapsulated and are not intended for
@@ -231,21 +221,19 @@ security.html.jspbconversions.safeStyleSheetToProto = function(styleSheet) {
  * message that has been received from an entity outside the application's
  * trust domain. Data coming from the browser is outside the application's trust
  * domain.
- * @param {!proto.webutil.html.types.SafeUrlProto} proto Protocol message to
+ * @param {!SafeUrlProto} proto Protocol message to
  *   convert from.
- * @return {!goog.html.SafeUrl}
+ * @return {!SafeUrl}
  * @suppress {visibility}
  */
-security.html.jspbconversions.safeUrlFromProto = function(proto) {
-  return goog.html.uncheckedconversions.
-      safeUrlFromStringKnownToSatisfyTypeContract(
-          goog.string.Const.from('From proto message. b/12014412'),
-          proto.getPrivateDoNotAccessOrElseSafeUrlWrappedValue() || '');
-};
-
+function safeUrlFromProto(proto) {
+  return uncheckedconversions.safeUrlFromStringKnownToSatisfyTypeContract(
+      Const.from('From proto message. b/12014412'),
+      proto.getPrivateDoNotAccessOrElseSafeUrlWrappedValue() || '');
+}
 
 /**
- * Returns a protocol message representation of the given goog.html.SafeUrl.
+ * Returns a protocol message representation of the given SafeUrl.
  *
  * Such a representation lets the SafeUrl type be preserved across protocol
  * buffer serialization and deserialization.
@@ -254,21 +242,20 @@ security.html.jspbconversions.safeUrlFromProto = function(proto) {
  * of the returned protocol message should be considered encapsulated and are
  * not intended for direct inspection or manipulation. Protocol messages can
  * be converted back into an instance of this type using
- * security.html.jspbconversions.safeUrlFromProto.
- * @param {!goog.html.SafeUrl} url SafeUrl to serialize.
- * @return {!proto.webutil.html.types.SafeUrlProto}
+ * safeUrlFromProto.
+ * @param {!SafeUrl} url SafeUrl to serialize.
+ * @return {!SafeUrlProto}
  */
-security.html.jspbconversions.safeUrlToProto = function(url) {
-  var protoUrl = new proto.webutil.html.types.SafeUrlProto();
-  var string = goog.html.SafeUrl.unwrap(url);
+function safeUrlToProto(url) {
+  var protoUrl = new SafeUrlProto();
+  var string = SafeUrl.unwrap(url);
   protoUrl.setPrivateDoNotAccessOrElseSafeUrlWrappedValue(string);
   return protoUrl;
-};
-
+}
 
 /**
- * Converts a protocol-message form of a goog.html.TrustedResourceUrl into a
- * goog.html.TrustedResourceUrl instance.
+ * Converts a protocol-message form of a TrustedResourceUrl into a
+ * TrustedResourceUrl instance.
  *
  * Protocol-message forms are intended to be opaque. The fields of the
  * protocol message should be considered encapsulated and are not intended for
@@ -280,23 +267,22 @@ security.html.jspbconversions.safeUrlToProto = function(url) {
  * message that has been received from an entity outside the application's
  * trust domain. Data coming from the browser is outside the application's trust
  * domain.
- * @param {!proto.webutil.html.types.TrustedResourceUrlProto} proto Protocol message to
+ * @param {!TrustedResourceUrlProto} proto Protocol message to
  *   convert from.
- * @return {!goog.html.TrustedResourceUrl}
+ * @return {!TrustedResourceUrl}
  * @suppress {visibility}
  */
-security.html.jspbconversions.trustedResourceUrlFromProto = function(proto) {
-  return goog.html.uncheckedconversions.
-      trustedResourceUrlFromStringKnownToSatisfyTypeContract(
-          goog.string.Const.from('From proto message. b/12014412'),
+function trustedResourceUrlFromProto(proto) {
+  return uncheckedconversions
+      .trustedResourceUrlFromStringKnownToSatisfyTypeContract(
+          Const.from('From proto message. b/12014412'),
           proto.getPrivateDoNotAccessOrElseTrustedResourceUrlWrappedValue() ||
               '');
-};
-
+}
 
 /**
  * Returns a protocol message representation of the given
- * goog.html.TrustedResourceUrl.
+ * TrustedResourceUrl.
  *
  * Such a representation lets the TrustedResourceUrl type be preserved across
  * protocol buffer serialization and deserialization.
@@ -305,13 +291,28 @@ security.html.jspbconversions.trustedResourceUrlFromProto = function(proto) {
  * of the returned protocol message should be considered encapsulated and are
  * not intended for direct inspection or manipulation. Protocol messages can
  * be converted back into an instance of this type using
- * security.html.jspbconversions.trustedResourceUrlFromProto.
- * @param {!goog.html.TrustedResourceUrl} url TrustedResourceUrl to serialize.
- * @return {!proto.webutil.html.types.TrustedResourceUrlProto}
+ * trustedResourceUrlFromProto.
+ * @param {!TrustedResourceUrl} url TrustedResourceUrl to serialize.
+ * @return {!TrustedResourceUrlProto}
  */
-security.html.jspbconversions.trustedResourceUrlToProto = function(url) {
-  var protoUrl = new proto.webutil.html.types.TrustedResourceUrlProto();
-  var string = goog.html.TrustedResourceUrl.unwrap(url);
+function trustedResourceUrlToProto(url) {
+  var protoUrl = new TrustedResourceUrlProto();
+  var string = TrustedResourceUrl.unwrap(url);
   protoUrl.setPrivateDoNotAccessOrElseTrustedResourceUrlWrappedValue(string);
   return protoUrl;
+}
+
+exports = {
+  safeHtmlFromProto,
+  safeHtmlToProto,
+  safeScriptFromProto,
+  safeScriptToProto,
+  safeStyleFromProto,
+  safeStyleSheetFromProto,
+  safeStyleSheetToProto,
+  safeStyleToProto,
+  safeUrlFromProto,
+  safeUrlToProto,
+  trustedResourceUrlFromProto,
+  trustedResourceUrlToProto,
 };
