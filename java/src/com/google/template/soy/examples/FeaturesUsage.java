9cebc912fafa272ff012e19c4a8088e558469fbd
Soy Authors <soy-dev@google.com>
2021-01-27 14:23:15 -0800

    More template import instead of FQN. Had to jump through some hoops to get the same soy file in examples/ with imports to compile in [] and OS.
    GITHUB_BREAKING_CHANGES=N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=354172330

--- java/src/com/google/template/soy/examples/FeaturesUsage.java
+++ java/src/com/google/template/soy/examples/FeaturesUsage.java
@@ -114,8 +114,8 @@ public class FeaturesUsage {
     SoyFileSet.Builder sfsBuilder = SoyFileSet.builder();
     SoyFileSet sfs =
         sfsBuilder
-            .add(Resources.getResource("simple.soy"))
-            .add(Resources.getResource("features.soy"))
+            .add(Resources.getResource("simple.soy"), "examples/simple.soy")
+            .add(Resources.getResource("features.soy"), "examples/features.soy")
             .setCompileTimeGlobals(Resources.getResource("FeaturesUsage_globals.txt"))
             .build();
     SoyTofu tofu = sfs.compileToTofu().forNamespace("soy.examples.features");
