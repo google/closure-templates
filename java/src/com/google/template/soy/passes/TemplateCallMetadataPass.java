aea478e9b809e19496149e56760ea23dbfa58104
Soy Authors <soy-dev@google.com>
2021-03-30 11:33:37 -0700

    Handle static Soy Element Composition calls used by [] templates (ex [1])
    Additional work would be needed to infer the source of dynamic calls.
    
    [1]: []
    
    GITHUB_BREAKING_CHANGES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=365858080

--- java/src/com/google/template/soy/passes/TemplateCallMetadataPass.java
+++ java/src/com/google/template/soy/passes/TemplateCallMetadataPass.java
@@ -18,13 +18,18 @@ package com.google.template.soy.passes;
 
 import static com.google.common.base.Strings.nullToEmpty;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.Lists.newArrayList;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Streams;
 import com.google.template.soy.base.internal.IdGenerator;
 import com.google.template.soy.exprtree.ExprNode;
 import com.google.template.soy.exprtree.ExprNode.Kind;
 import com.google.template.soy.exprtree.FieldAccessNode;
 import com.google.template.soy.exprtree.ItemAccessNode;
+import com.google.template.soy.exprtree.MethodCallNode;
+import com.google.template.soy.exprtree.RecordLiteralNode;
+import com.google.template.soy.exprtree.TemplateLiteralNode;
 import com.google.template.soy.exprtree.VarDefn;
 import com.google.template.soy.exprtree.VarRefNode;
 import com.google.template.soy.soytree.CallBasicNode;
@@ -33,17 +38,21 @@ import com.google.template.soy.soytree.CallNode;
 import com.google.template.soy.soytree.CallParamNode;
 import com.google.template.soy.soytree.CallParamValueNode;
 import com.google.template.soy.soytree.ForNonemptyNode;
+import com.google.template.soy.soytree.HtmlTagNode;
 import com.google.template.soy.soytree.LetValueNode;
 import com.google.template.soy.soytree.SoyFileNode;
 import com.google.template.soy.soytree.SoyNode;
 import com.google.template.soy.soytree.SoyNode.LocalVarNode;
 import com.google.template.soy.soytree.SoyTreeUtils;
+import com.google.template.soy.soytree.TagName;
 import com.google.template.soy.soytree.TemplateNode;
 import com.google.template.soy.soytree.defn.LocalVar;
 import com.google.template.soy.templatecall.TemplateCallMetadata;
+import java.util.List;
+import java.util.Optional;
 
 /** Provides a serializable proto containing descriptions of template calls. */
-@RunAfter(ResolveTemplateNamesPass.class)
+@RunAfter(ResolveTemplateNamesPass.class) // Required to use getCalleeName()
 public final class TemplateCallMetadataPass implements CompilerFileSetPass {
 
   TemplateCallMetadataPass() {}
@@ -52,6 +61,13 @@ public final class TemplateCallMetadataPass implements CompilerFileSetPass {
   public Result run(ImmutableList<SoyFileNode> sourceFiles, IdGenerator nodeIdGen) {
     for (SoyFileNode file : sourceFiles) {
       for (TemplateNode template : file.getTemplates()) {
+        // Handle Soy Element Composition
+        List<TemplateCallMetadata.TemplateCall> callsFromTags =
+            SoyTreeUtils.getAllNodesOfType(template, HtmlTagNode.class).stream()
+                .map(TemplateCallMetadataPass::calculateTemplateCallInfoFromTag)
+                .flatMap(Streams::stream)
+                .collect(toImmutableList());
+
         template.addTemplateCallMetadata(
             TemplateCallMetadata.Template.newBuilder()
                 .setName(template.getTemplateName())
@@ -60,10 +76,69 @@ public final class TemplateCallMetadataPass implements CompilerFileSetPass {
                     SoyTreeUtils.getAllNodesOfType(template, CallNode.class).stream()
                         .map(TemplateCallMetadataPass::calculateTemplateCall)
                         .collect(toImmutableList()))
+                .addAllCalls(callsFromTags)
                 .build());
       }
     }
     return Result.CONTINUE;
+  }
+
+  /**
+   * Parses template calls which are declared via tag syntax.
+   *
+   * <p>This logic only infers static calls where the binding is produced by the framework. This
+   * handles framework-provided Wiz Component template calls which use Soy element composition. A
+   * more complex solution may be needed for dynamic calls.
+   *
+   * @param tagNode The node that may reference a template call
+   * @return template call node
+   */
+  private static Optional<TemplateCallMetadata.TemplateCall> calculateTemplateCallInfoFromTag(
+      HtmlTagNode tagNode) {
+    TagName tagName = tagNode.getTagName();
+    if (tagNode.getTagName().isStatic()) {
+      return Optional.empty();
+    }
+    ExprNode tagExprNode = tagName.getDynamicTagName().getExpr().getRoot();
+    if (tagExprNode instanceof TemplateLiteralNode) {
+      // can determine that the template is called, but not its param args
+      // TODO(b/179912526): can this syntax specify param args? if so, add corresponding test case
+      String destTemplateName = ((TemplateLiteralNode) tagExprNode).getResolvedName();
+      return Optional.of(
+          TemplateCallMetadata.TemplateCall.newBuilder()
+              .setDestTemplateName(destTemplateName)
+              .build());
+    }
+
+    if (!(tagExprNode.getKind() == ExprNode.Kind.METHOD_CALL_NODE
+        && ((MethodCallNode) tagExprNode).getMethodName().identifier().equals("bind"))) {
+      return Optional.empty();
+    }
+
+    MethodCallNode bind = (MethodCallNode) tagExprNode;
+    if (bind.getChild(0).getKind() != ExprNode.Kind.TEMPLATE_LITERAL_NODE) {
+      return Optional.empty();
+    }
+    RecordLiteralNode recordLiteralNode = (RecordLiteralNode) bind.getParams().get(0);
+    List<ExprNode> bindExprs = recordLiteralNode.getChildren();
+    List<TemplateCallMetadata.ParamArg> callParamArgs = newArrayList();
+    for (int i = 0; i < bindExprs.size(); i++) {
+      callParamArgs.add(
+          TemplateCallMetadata.ParamArg.newBuilder()
+              .setKey(recordLiteralNode.getKey(i).identifier())
+              .setVarRef(
+                  resolveLocalVarRefToParamRef(
+                      bindExprs.get(i), TemplateCallMetadata.VarRefInfo.newBuilder()))
+              .build());
+    }
+    String destTemplateName = ((TemplateLiteralNode) bind.getChild(0)).getResolvedName();
+    return Optional.of(
+        TemplateCallMetadata.TemplateCall.newBuilder()
+            .setDestTemplateName(destTemplateName)
+            // TODO(b/179912526): when / if needed, handle soy element composition + delcalls here
+            .setIsDelcall(false)
+            .addAllParamArgs(callParamArgs)
+            .build());
   }
 
   private static TemplateCallMetadata.TemplateCall calculateTemplateCall(
