799b88c674fe943a5a05b9889e28915a13a02b8c
Soy Authors <soy-dev@google.com>
2021-06-02 09:00:28 -0700

    A few copyright fixes for copybara.
    GITHUB_BREAKING_CHANGES=N/A
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=377070773

--- java/src/com/google/template/soy/jbcsrc/TemplateAnalysis.java
+++ java/src/com/google/template/soy/jbcsrc/TemplateAnalysis.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016   Google Inc.
+ * Copyright 2016 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  */
 
 package com.google.template.soy.jbcsrc;
-
 
 import com.google.template.soy.exprtree.DataAccessNode;
 import com.google.template.soy.exprtree.VarRefNode;
