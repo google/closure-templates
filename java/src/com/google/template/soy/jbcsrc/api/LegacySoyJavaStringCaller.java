54f4562901c7d839486c5d5d2f2b36ffde3faaef
Soy Authors <soy-dev@google.com>
2021-08-03 10:21:48 -0700

    internal
    PiperOrigin-RevId: 388486039

--- java/src/com/google/template/soy/jbcsrc/api/LegacySoyJavaStringCaller.java
+++ java/src/com/google/template/soy/jbcsrc/api/LegacySoyJavaStringCaller.java
@@ -20,9 +20,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import javax.inject.Qualifier;
 
-@Qualifier
 @Target(ElementType.METHOD)
 @Retention(RUNTIME)
 public @interface LegacySoyJavaStringCaller {};
