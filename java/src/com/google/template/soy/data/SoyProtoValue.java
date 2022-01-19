8208cc39e4cd76c5726535f3e524eabfc27ae5d6
Soy Authors <soy-dev@google.com>
2022-01-19 09:53:29 -0800

    Internal change
    PiperOrigin-RevId: 422838763

--- java/src/com/google/template/soy/data/SoyProtoValue.java
+++ java/src/com/google/template/soy/data/SoyProtoValue.java
@@ -370,7 +370,7 @@ public final class SoyProtoValue extends SoyAbstractValue implements SoyLegacyOb
 
   @Override
   public void render(LoggingAdvisingAppendable appendable) throws IOException {
-    TextFormat.print(proto, appendable);
+    TextFormat.printer().print(proto, appendable);
   }
 
   // -----------------------------------------------------------------------------------------------
