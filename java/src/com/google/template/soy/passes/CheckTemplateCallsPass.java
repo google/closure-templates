205cddf60441e11319a9a024890acaaf4cabd141
Soy Authors <soy-dev@google.com>
2022-07-27 11:57:25 -0700

    deltemplate passlist burndown.
    PiperOrigin-RevId: 463643406

--- java/src/com/google/template/soy/passes/CheckTemplateCallsPass.java
+++ java/src/com/google/template/soy/passes/CheckTemplateCallsPass.java
@@ -242,7 +242,6 @@ public final class CheckTemplateCallsPass implements CompilerFileSetPass {
           // $ grep allowemptydefault /tmp/delcalls.txt | awk '{printf "\"%s\",\n", $3}' | sort |
           // uniq | grep -v '^"boq.dpanelchromeui.'
           "boq.accountsettingsnotifications.securityevent.templates.eventDetails",
-          "boq.accounts.wireframe.api.nameinput.templates.isDense",
           "boq.ads.townsquare.marketplaceui.components.componentregistry.classes.templates.type",
           "boq.androidgantryui.common.local.templates.localBanner",
           "boq.dasheradminconsolefrontendshell.landing.leftnav.globalparamsmod.templates.globalParamsForCollapsed",
