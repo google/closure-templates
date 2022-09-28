7d62fd47490e72779266a4fa59ad84d92ce174bc
Soy Authors <soy-dev@google.com>
2022-09-27 19:56:23 -0700

    deltemplate passlist burndown.
    PiperOrigin-RevId: 477337742

--- java/src/com/google/template/soy/passes/CheckTemplateCallsPass.java
+++ java/src/com/google/template/soy/passes/CheckTemplateCallsPass.java
@@ -171,8 +171,6 @@ public final class CheckTemplateCallsPass implements CompilerFileSetPass {
 
   private static final ImmutableSet<String> DEFAULT_DELTEMPLATE_PASSLIST =
       ImmutableSet.of(
-          "boq.dynamitewebui.member.templates.isOneOnOneDm", // circular dep
-          "boq.dynamitewebui.member.templates.readSingleUserFromDmHumanMembersList", // circular dep
           "boq.saveui.listitem.templates.cardHeader", // circualr deps, cl/455220729 breaks tests
           "boq.shopping.property.ui.components.componentregistry.ghost.templates.type", // cir dep
           "boq.shopping.property.ui.components.componentregistry.templates.type", // circular dep
