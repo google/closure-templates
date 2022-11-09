40712f39040a86a9405d0fbdac61f1ceb7900b01
Jesse Costello-Good <jcg@google.com>
2022-11-09 11:37:05 -0800

    Internal
    PiperOrigin-RevId: 487303537

--- java/src/com/google/template/soy/jssrc/dsl/ParamDecls.java
+++ java/src/com/google/template/soy/jssrc/dsl/ParamDecls.java
@@ -34,25 +34,44 @@ public abstract class ParamDecls {
 
   abstract ImmutableList<ParamDecl> params();
 
-  public static ParamDecls create(List<ParamDecl> params) {
-    return new AutoValue_ParamDecls(ImmutableList.copyOf(params));
+  abstract boolean templateStyle();
+
+  public static ParamDecls createForTemplate(List<ParamDecl> params) {
+    return new AutoValue_ParamDecls(ImmutableList.copyOf(params), true);
+  }
+
+  public static ParamDecls createForExtern(List<ParamDecl> params) {
+    return new AutoValue_ParamDecls(ImmutableList.copyOf(params), false);
   }
 
   public String getCode() {
+    if (templateStyle()) {
+      if (params().isEmpty()) {
+        return "{} : {}";
+      }
 
-    if (params().isEmpty()) {
-      return "{} : {}";
+      // Generate the dict of param names (e.g. "{amount, name = ‘Vesper’}"). Default values are not
+      // supported yet.
+      String paramNamesDict =
+          "{" + params().stream().map(ParamDecl::nameDecl).collect(joining(", ")) + "}";
+
+      // Generate the dict of param types (e.g. "{amount: number, name?: string}").
+      String paramTypesDict =
+          "{" + params().stream().map(ParamDecl::typeDecl).collect(joining(", ")) + "}";
+
+      return paramNamesDict + " : " + paramTypesDict;
+    } else {
+      StringBuilder sb = new StringBuilder();
+      boolean first = true;
+      for (ParamDecl param : params()) {
+        if (first) {
+          first = false;
+        } else {
+          sb.append(", ");
+        }
+        sb.append(param.name()).append(": ").append(param.type());
+      }
+      return sb.toString();
     }
-
-    // Generate the dict of param names (e.g. "{amount, name = ‘Vesper’}"). Default values are not
-    // supported yet.
-    String paramNamesDict =
-        "{" + params().stream().map(ParamDecl::nameDecl).collect(joining(", ")) + "}";
-
-    // Generate the dict of param types (e.g. "{amount: number, name?: string}").
-    String paramTypesDict =
-        "{" + params().stream().map(ParamDecl::typeDecl).collect(joining(", ")) + "}";
-
-    return paramNamesDict + " : " + paramTypesDict;
   }
 }
