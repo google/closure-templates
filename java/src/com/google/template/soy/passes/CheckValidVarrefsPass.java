e20564212ff7a4e5d62b000429ffafe77bdb810b
Jesse Costello-Good <jcg@google.com>
2024-08-20 09:43:20 -0700

    Update pass for most recent types.
    PiperOrigin-RevId: 665403170

--- java/src/com/google/template/soy/passes/CheckValidVarrefsPass.java
+++ java/src/com/google/template/soy/passes/CheckValidVarrefsPass.java
@@ -16,25 +16,18 @@
 
 package com.google.template.soy.passes;
 
-import com.google.common.collect.ImmutableSet;
 import com.google.template.soy.base.internal.IdGenerator;
 import com.google.template.soy.error.ErrorReporter;
 import com.google.template.soy.error.SoyErrorKind;
 import com.google.template.soy.exprtree.ExprNode.Kind;
 import com.google.template.soy.exprtree.ExprNode.ParentExprNode;
-import com.google.template.soy.exprtree.VarDefn;
 import com.google.template.soy.exprtree.VarRefNode;
 import com.google.template.soy.soytree.SoyFileNode;
 import com.google.template.soy.soytree.SoyTreeUtils;
-import com.google.template.soy.types.SoyType;
 
 /** Reports errors for illegal symbol references. */
 @RunAfter({ResolveExpressionTypesPass.class, MoreCallValidationsPass.class})
 final class CheckValidVarrefsPass implements CompilerFilePass {
-
-  private static final ImmutableSet<SoyType.Kind> BAD_SOY_TYPE_KINDS =
-      ImmutableSet.of(
-          SoyType.Kind.TEMPLATE_MODULE, SoyType.Kind.PROTO_ENUM_TYPE, SoyType.Kind.PROTO_MODULE);
 
   private static final SoyErrorKind ILLEGAL_TYPE_OF_VARIABLE =
       SoyErrorKind.of("Illegal use of symbol ''{0}''.");
@@ -51,53 +44,58 @@ final class CheckValidVarrefsPass implements CompilerFilePass {
   }
 
   private void checkVarRef(VarRefNode varRef) {
-    VarDefn defn = varRef.getDefnDecl();
-    if (defn != null && defn.hasType()) {
-      if (BAD_SOY_TYPE_KINDS.contains(defn.type().getKind())) {
-        errorReporter.report(
-            varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
-      }
-    }
-
     ParentExprNode parent = varRef.getParent();
     Kind parentKind = parent != null ? parent.getKind() : null;
-    SoyType.Kind exprKind = varRef.getType().getKind();
-    if (exprKind == SoyType.Kind.FUNCTION
-        && !(parentKind == Kind.FUNCTION_NODE && varRef.equals(parent.getChild(0)))) {
-      // Using an extern/function name anywhere other than as the base of a function call.
-      errorReporter.report(
-          varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
-    } else if (exprKind == SoyType.Kind.PROTO_TYPE
-        && !(parent == null
+
+    switch (varRef.getType().getKind()) {
+      case TEMPLATE_MODULE:
+      case PROTO_ENUM_TYPE:
+      case PROTO_MODULE:
+      case CSS_MODULE:
+      case CSS_TYPE:
+      case TOGGLE_TYPE:
+        errorReporter.report(
+            varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
+        break;
+      case FUNCTION:
+        if (!(parentKind == Kind.FUNCTION_NODE && varRef.equals(parent.getChild(0)))) {
+          // Using an extern/function name anywhere other than as the base of a function call.
+          errorReporter.report(
+              varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
+        }
+        break;
+      case PROTO_TYPE:
+        if (!(parent == null
             || parentKind == Kind.FIELD_ACCESS_NODE
             || parentKind == Kind.FUNCTION_NODE)) {
-      // This is for imports like:
-      // import {MyMessage} from 'path/to/my/proto/file.proto';
-      // These are only allowed in:
-      // - Proto init calls (i.e. `MyMessage()`). In this case, the parent is null (the VarRefNode
-      //   is the `nameExpr` on the FunctionNode).
-      // - Field accesses, like `MyMessage.MySubMessage`
-      // - `ve_def` data proto type parameters (i.e. `ve_def('MyVe', 123, MyMessage)`). However,
-      //   this allows any FunctionNode, not just `ve_def`, to prevent duplicate/confusing errors on
-      //   things like misspelling `ve_def`. Function parameters are type checked elsewhere, which
-      //   which will report any issues.
-      errorReporter.report(
-          varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
-    } else if (exprKind == SoyType.Kind.PROTO_EXTENSION && parentKind != Kind.METHOD_CALL_NODE) {
-      // This is for imports like:
-      // import {TopLevelEnum} from 'path/to/my/proto/file.proto';
-      // These are only allowed as the parameter to the `getExtension`, `getReadonlyExtension` and
-      // `hasExtension` proto methods. However, this allows any MethodCallNode to prevent
-      // duplicate/confusing errors on things like misspelling one of the method names. Method
-      // parameters are type checked elsewhere, which which will report any issues.
-      errorReporter.report(
-          varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
-    } else if (exprKind == SoyType.Kind.CSS_TYPE && parentKind != Kind.FIELD_ACCESS_NODE) {
-      // This is for imports like:
-      // import {classes} from 'path/to/my/css/file/css';
-      // These are only allowed in field accesses, like `classes.myStyleClassName`
-      errorReporter.report(
-          varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
+          // This is for imports like:
+          // import {MyMessage} from 'path/to/my/proto/file.proto';
+          // These are only allowed in:
+          // - Proto init calls (i.e. `MyMessage()`). In this case, the parent is null (the
+          // VarRefNode is the `nameExpr` on the FunctionNode).
+          // - Field accesses, like `MyMessage.MySubMessage`
+          // - `ve_def` data proto type parameters (i.e. `ve_def('MyVe', 123, MyMessage)`). However,
+          //   this allows any FunctionNode, not just `ve_def`, to prevent duplicate/confusing
+          //   errors on things like misspelling `ve_def`. Function parameters are type checked
+          //   elsewhere, which will report any issues.
+          errorReporter.report(
+              varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
+        }
+        break;
+      case PROTO_EXTENSION:
+        if (parentKind != Kind.METHOD_CALL_NODE) {
+          // This is for imports like:
+          // import {TopLevelEnum} from 'path/to/my/proto/file.proto';
+          // These are only allowed as the parameter to the `getExtension`, `getReadonlyExtension`
+          // and `hasExtension` proto methods. However, this allows any MethodCallNode to prevent
+          // duplicate/confusing errors on things like misspelling one of the method names. Method
+          // parameters are type checked elsewhere, which which will report any issues.
+          errorReporter.report(
+              varRef.getSourceLocation(), ILLEGAL_TYPE_OF_VARIABLE, varRef.toSourceString());
+        }
+        break;
+      default:
+        break;
     }
   }
 }
