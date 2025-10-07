4d542220d9eff5b2ff29575e5949bcdf00d7b83c
Nicholas Yu <nicholasyu@google.com>
2025-10-07 10:32:03 -0700

    Internal
    PiperOrigin-RevId: 816275913

--- java/src/com/google/template/soy/jbcsrc/shared/ContextStore.java
+++ java/src/com/google/template/soy/jbcsrc/shared/ContextStore.java
@@ -7,7 +7,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, softwarejava int
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,35 +16,74 @@
 
 package com.google.template.soy.jbcsrc.shared;
 
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableMap;
 import com.google.template.soy.data.SoyValue;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Map;
+import java.util.NoSuchElementException;
 import javax.annotation.Nullable;
 
 /** Context API */
-public final class ContextStore {
+public interface ContextStore {
 
-  Deque<Map<String, SoyValue>> contextStack = new ArrayDeque<>();
+  public void pushContext(ImmutableMap<String, SoyValue> childValues);
 
-  public ContextStore() {}
-
-  public void pushContext(Map<String, SoyValue> context) {
-    contextStack.addFirst(context);
-  }
-
-  public void popContext() {
-    contextStack.removeFirst();
-  }
+  public void popContext();
 
   @Nullable
-  public SoyValue getContextValue(String key) {
-    for (Map<String, SoyValue> context : contextStack) {
-      SoyValue value = context.get(key);
-      if (value != null) {
-        return value;
-      }
+  public SoyValue getContextValue(String key);
+
+  /** One context node. */
+  @AutoValue
+  abstract static class ContextNode {
+    abstract ImmutableMap<String, SoyValue> values();
+
+    @Nullable
+    abstract ContextNode parent();
+
+    static ContextNode createRoot() {
+      return new AutoValue_ContextStore_ContextNode(ImmutableMap.of(), null);
     }
-    return null;
+
+    @Nullable
+    SoyValue getContextValue(String key) {
+      if (values().containsKey(key)) {
+        return values().get(key);
+      }
+      return parent() == null ? null : parent().getContextValue(key);
+    }
+
+    ContextNode pushContext(ImmutableMap<String, SoyValue> childValues) {
+      return new AutoValue_ContextStore_ContextNode(childValues, this);
+    }
+
+    ContextNode popContext() {
+      if (parent() == null) {
+        throw new NoSuchElementException("ContextNode underrun");
+      }
+      return parent();
+    }
+  }
+
+  /** Test ContextStore */
+  public static class TestContextStore implements ContextStore {
+    private ContextNode currentContext = ContextNode.createRoot();
+
+    public TestContextStore() {}
+
+    @Override
+    public void pushContext(ImmutableMap<String, SoyValue> childValues) {
+      currentContext = currentContext.pushContext(childValues);
+    }
+
+    @Override
+    public void popContext() {
+      currentContext = currentContext.popContext();
+    }
+
+    @Override
+    @Nullable
+    public SoyValue getContextValue(String key) {
+      return currentContext.getContextValue(key);
+    }
   }
 }
