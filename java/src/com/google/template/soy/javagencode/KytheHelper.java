654193f1043b80672db5f8d5e9de7171550daeab
Mikita Belahlazau <nbeloglazov@google.com>
2026-08-08 12:32:22 -0700

    Emit GeneratedCodeInfo header in Soy TSX code generator.
    Updates Soy TSX header generation to emit kythe.proto.metadata.GeneratedCodeInfo instead of legacy kythe-inline-metadata. Updates GenDtsCodeVisitor to invoke appendGeneratedCodeInfoForDts and fixes the edge direction in KytheHelper mappingRule to /kythe/edge/imputes so analyzer_driver can centrally process metadata for TSX declarations.
    
    PiperOrigin-RevId: 961483606

--- java/src/com/google/template/soy/javagencode/KytheHelper.java
+++ java/src/com/google/template/soy/javagencode/KytheHelper.java
@@ -27,16 +27,30 @@ import javax.annotation.Nullable;
 /** Helper for constructing GeneratedCodeInfo proto */
 public class KytheHelper {
 
+  public static final String DEFAULT_COMMENT_PREFIX = "kythe-inline-metadata";
+  public static final String GENERATED_CODE_INFO_COMMENT_PREFIX =
+      "kythe.proto.metadata.GeneratedCodeInfo";
+
   private final String kytheCorpus;
   private final SourceFilePath sourceFilePath;
+  private final String commentPrefix;
 
   public KytheHelper(SourceFilePath sourceFilePath) {
-    this("", sourceFilePath);
+    this(sourceFilePath, DEFAULT_COMMENT_PREFIX);
+  }
+
+  public KytheHelper(SourceFilePath sourceFilePath, String commentPrefix) {
+    this("", sourceFilePath, commentPrefix);
   }
 
   public KytheHelper(String kytheCorpus, SourceFilePath sourceFilePath) {
+    this(kytheCorpus, sourceFilePath, DEFAULT_COMMENT_PREFIX);
+  }
+
+  public KytheHelper(String kytheCorpus, SourceFilePath sourceFilePath, String commentPrefix) {
     this.kytheCorpus = kytheCorpus;
     this.sourceFilePath = Preconditions.checkNotNull(sourceFilePath);
+    this.commentPrefix = Preconditions.checkNotNull(commentPrefix);
   }
 
   @Nullable
