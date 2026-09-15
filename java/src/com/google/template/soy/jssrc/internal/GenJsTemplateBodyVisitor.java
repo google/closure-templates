10d36b81121ea1bd07b7afa10688c9ecc106b24f
Robert Vargas <robvargas@google.com>
2026-09-15 15:40:55 -0700

    Allow {let} to mask {@param}.
    PiperOrigin-RevId: 982082640

--- java/src/com/google/template/soy/jssrc/internal/GenJsTemplateBodyVisitor.java
+++ java/src/com/google/template/soy/jssrc/internal/GenJsTemplateBodyVisitor.java
@@ -75,7 +75,6 @@ import com.google.template.soy.soytree.MsgHtmlTagNode;
 import com.google.template.soy.soytree.MsgPlaceholderNode;
 import com.google.template.soy.soytree.PrintNode;
 import com.google.template.soy.soytree.ReturnNode;
-import com.google.template.soy.soytree.SoyFileNode;
 import com.google.template.soy.soytree.SoyNode;
 import com.google.template.soy.soytree.SoyNode.ParentSoyNode;
 import com.google.template.soy.soytree.SoyNode.RenderUnitNode;
@@ -629,7 +628,6 @@ public class GenJsTemplateBodyVisitor extends AbstractReturningSoyNodeVisitor<St
       Expression limit,
       Function<Expression, Expression> getDataItemFunction) {
     // Build some local variable names.
-    String refPrefix = node.getVarRefName();
     String jsLetPrefix = node.getVarName() + node.getForNodeId();
 
     // TODO(b/32224284): A more consistent pattern for local variable management.
@@ -648,7 +646,7 @@ public class GenJsTemplateBodyVisitor extends AbstractReturningSoyNodeVisitor<St
     try (var scope = templateTranslationContext.enterSoyAndJsScope()) {
 
       // Populate the local var translations with the translations from this node.
-      templateTranslationContext.soyToJsVariableMappings().put(refPrefix, id(dataName));
+      templateTranslationContext.soyToJsVariableMappings().put(node.getVar(), id(dataName));
 
       Id loopIndexId = Id.create(loopIndexName);
       if (node.getIndexVar() != null) {
